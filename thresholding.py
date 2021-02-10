daa6d848771023271e3e6fec2f15b082d257cdd1
Amritanshu Singh <64220796+singhamritanshu@users.noreply.github.com>
2021-02-10 10:41:50 +0530

    Added adaptive thresholding

--- thresholding.py
+++ thresholding.py
@@ -12,6 +12,13 @@ cv.imshow("Simple Thresh",thresh)
 # Creating an inverse thresholded image 
 # All the pixel values which are less than 150 (threshold value) is set to 255 which is white where as all the pixel values greater than 150(threshold value) is set to 0 black.
 threshold, thresh_inv = cv.threshold(gray,150,255,cv.THRESH_BINARY_INV)
-cv.imshow("Inverse Thresholded",thresh_inv) 
+cv.imshow("Simple Inverse Thresholded",thresh_inv) 
 
+# Adaptive thresholding: In this we let the computer find and decide the optimal thresholding value for us.
+# We are using mean in type of thresholding to calculate the optimal threshlod value.
+# 7 is the kernel size used by the opencv to calculat ethe mean and the 3 is the c value which will subtracted from the mean to fine tune our optimal threshold value 
+adaptive_thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY,7,3) 
+cv.imshow("Adaptive threshol",adaptive_thresh)
+adaptive_thresh_inv = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_MEAN_C,cv.THRESH_BINARY_INV,13,7) 
+cv.imshow("Adaptive thresh_inv",adaptive_thresh_inv)
 cv.waitKey(0)
